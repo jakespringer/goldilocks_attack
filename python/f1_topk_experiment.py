b9948b259eee4672d760a4a5c56b321829047066
jspring <jspringer@csail.mit.edu>
2020-07-21 14:05:09 -0400

    add __name__ guard for f1_topk_experiment

--- python/f1_topk_experiment.py
+++ python/f1_topk_experiment.py
@@ -1,4 +1,7 @@
+# a quick helper script to reproduce results from the paper
+# left as a reference for how to automate the automate script
 import subprocess
 
-for i in [100, 1097, 1808, 2981, 4915, 8103, 13360, 22026, 36316, 59874, 98716, 162755]
-    subprocess.run(['python', 'automate.py', '--topk', str(i), '--dictionary', 'l2'])
+if __name__ == '__main__':
+    for i in [100, 1097, 1808, 2981, 4915, 8103, 13360, 22026, 36316, 59874, 98716, 162755]
+        subprocess.run(['python', 'automate.py', '--topk', str(i), '--dictionary', 'l2'])
